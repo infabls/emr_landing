1ba6828ff502ed44f9bf4dff23b25ccb6e55729e
infabls <infabls.com@gmail.com>
2021-04-13 15:23:11 +0600

    add analytics

--- includes/footer.php
+++ includes/footer.php
@@ -74,6 +74,30 @@
 <script src="https://emr.kz/style/js/retina.js"></script> 
 <script src="https://emr.kz/style/js/google-code-prettify/prettify.js"></script> 
 <script src="https://emr.kz/style/js/scripts.js"></script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(75605467, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/75605467" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ64CDN4J7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GQ64CDN4J7');
+</script>
     <script id="bx24_form_button" data-skip-moving="true">
       (function(w,d,u,b){w['Bitrix24FormObject']=b;w[b] = w[b] || function(){arguments[0].ref=u;
           (w[b].forms=w[b].forms||[]).push(arguments[0])};
